06f77c69c50aa412d7736665fbd92fc56264464e
Vivek Mishra <vivek.mishra@cdk.com>
2019-09-11 12:19:57 +0530

    Organized angular 2 codebase

--- spring-security-angular-ui/src/app.module.ts
+++ spring-security-angular-ui/src/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
