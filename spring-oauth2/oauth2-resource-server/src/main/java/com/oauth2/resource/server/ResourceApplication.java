03096a877cca82ae51d5f7b9d4e14a7285d30f4b
vivekprm <vivek.pr.mi@gmail.com>
2019-09-17 08:22:21 +0530

    Added oauth2 implementation

--- spring-oauth2/oauth2-resource-server/src/main/java/com/oauth2/resource/server/ResourceApplication.java
+++ spring-oauth2/oauth2-resource-server/src/main/java/com/oauth2/resource/server/ResourceApplication.java
@@ -1,8 +1,7 @@
-package com.oauth2.server;
+package com.oauth2.resource.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
 @SpringBootApplication
-@EnableAuthorizationServer
 @RestController
 @EnableResourceServer
-public class AuthServerApplication  {
-    @RequestMapping("/user")
-    public Principal user(Principal user) {
-        return user;
+public class ResourceApplication {
+    @RequestMapping("/api/greeting")
+    public Greeting greeting() {
+        return new Greeting("Hello oauth2 backend");
     }
+
     public static void main(String[] args) {
-        SpringApplication.run(AuthServerApplication.class, args);
+        SpringApplication.run(ResourceApplication.class, args);
     }
 }
