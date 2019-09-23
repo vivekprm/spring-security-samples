00fd644dcc9663b6956e591152c34462e9d6ceb6
Vivek Mishra <vivek.mishra@cdk.com>
2019-09-23 12:09:12 +0530

    Added login at gateway level

--- multi-ui-gateway/multi-ui-uiserver/src/main/java/com/multiuigateway/ui/MultiUiGatewayUiApplication.java
+++ multi-ui-gateway/multi-ui-uiserver/src/main/java/com/multiuigateway/ui/MultiUiGatewayUiApplication.java
@@ -2,11 +2,21 @@ package com.multiuigateway.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class MultiUiGatewayUiApplication {
+    @RequestMapping("/user")
+    public Map<String, String> user(Principal user) {
+        return Collections.singletonMap("name", user.getName());
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MultiUiGatewayUiApplication.class, args);
     }
